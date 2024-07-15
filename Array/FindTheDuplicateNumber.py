cf526dce150d4540b35df9798aa6ff6238e1c069
Navya Jain <89401003+naavy-jain@users.noreply.github.com>
2024-07-15 17:10:42 +0530

    Create FindTheDuplicateNumber.py

---
+++ Array/FindTheDuplicateNumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        dic={}
+        for i in nums:
+            if i in dic:
+                return i
+            else:
+                dic[i]=1
