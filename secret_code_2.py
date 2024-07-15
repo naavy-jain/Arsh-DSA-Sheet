e546454b43ef9f087f170f854a3e8edc3e795daa
Navya Jain <89401003+naavy-jain@users.noreply.github.com>
2024-07-15 16:56:17 +0530

    Delete secret_code_2.py

--- secret_code_2.py
+++
@@ -1,7 +0,0 @@
-# Using MD5 hash of 32 bit strings
-import random
-import string
-import pwinput
-user_input = input("Enter string: ")
-user_input = pwinput.pwinput(prompt="Enter string", mask=" * ")
-user_input = ord("Hello !")
